541ce1d98aa0f0737caf2b47f32bb63b022a8ce8
Roderik van Heijst <rmvh@0x0a.nl>
2021-06-29 20:46:09 +0200

    Make TodoRepository singleton (succeeds)

--- examples/todo/src/repositories/todo.repository.ts
+++ examples/todo/src/repositories/todo.repository.ts
@@ -14,6 +14,7 @@ import {Todo, TodoRelations, Person, Assignment} from '../models';
 import {AssignmentRepository} from './assignment.repository';
 import {PersonRepository} from './person.repository';
 
+@bind({scope: BindingScope.SINGLETON})
 export class TodoRepository extends DefaultCrudRepository<
   Todo,
   typeof Todo.prototype.id,
